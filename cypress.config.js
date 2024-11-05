e8b30a2308d44f8c3c246b80e639878e32656ee3
Kazal Chanra Barman <kcb.brurcse42@gmail.com>
2024-11-05 13:35:26 +0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('cypress');
-const { addMochawesome } = require('cypress/plugin');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
@@ -13,9 +12,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.demoblaze.com',
     setupNodeEvents(on, config) {
-      // Ensure the Mochawesome plugin is added correctly
-      addMochawesome(on);
-      return config; // Return the config object to avoid issues
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
+
