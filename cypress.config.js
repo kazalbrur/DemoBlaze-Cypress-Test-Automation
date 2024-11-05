d6e30741137c8f2298141a3d4b36c5849e4be493
Kazal Chanra Barman <kcb.brurcse42@gmail.com>
2024-11-05 12:22:38 +0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 15000,
-  requestTimeout: 15000,
-  responseTimeout: 15000,
   reporter: 'cypress-mochawesome-reporter',
-  video: false,
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
 
   e2e: {
     baseUrl: 'https://www.demoblaze.com',
